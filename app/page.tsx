fcec112697d7a8d393d3772f0cf1a1696229d242
Amiel Mir Ordas <miramielordas@gmail.com>
2025-05-08 21:55:48 +0800

    Update root page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Navbar from "@/components/Navbar";
-import MultiCardLayout from "../components/Multicard";
-import WelcomeArea from "../components/WelcomeArea";
+import Navbar from "@/components/Home Page/Navbar";
+import MultiCardLayout from "../components/Home Page/Multicard";
+import WelcomeArea from "../components/Home Page/WelcomeArea";
 import AnimationWrapper from "@/components/wrappers/PageAnimation";
-import Footer from "@/components/Footer";
+import Footer from "@/components/Home Page/Footer";
 export default function Home() {
   return (
     <AnimationWrapper>
